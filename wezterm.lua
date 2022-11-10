81cb57f893901c17c2a0c439998c156168cf9727
Nicolo Maioli <nicolomaioli@gmail.com>
2022-11-10 17:59:49 +0000

    First payload

---
+++ wezterm.lua
@@ -0,0 +1,15 @@
+local wezterm = require('wezterm')
+
+local M = {
+    color_scheme = 'tokyonight-storm',
+    font = wezterm.font_with_fallback {
+        {family = 'FiraCode Nerd Font', weight = 'Medium'},
+    },
+    font_size = 11,
+    enable_scroll_bar = false,
+    hide_tab_bar_if_only_one_tab = true,
+    window_background_opacity = 1.0,
+    window_close_confirmation = 'NeverPrompt',
+}
+
+return M
